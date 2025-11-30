91225a790278c692c9000c5ae91bf5adc9fc1f7c
Saroj-kr-tharu <sarojkumartharu11234@gmail.com>
2025-11-30 11:53:04 +0545

    added cors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require("cors");
 
 
 const {PORT}= require('./config/serverConfig')
@@ -10,7 +11,25 @@ const serverSetupAndStart = async () => {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}))
 
-   
+    // app.use(
+    // cors({
+    //     origin: function (origin, callback) {
+    //     const allowedOrigins = ['http://localhost:4200'];
+    //     if (!origin || allowedOrigins.includes(origin)) {
+    //         callback(null, true);
+    //     } else {
+    //         callback(new Error('Not allowed by CORS'));
+    //     }
+    //     },
+    //     credentials: true,
+    //     methods: ['GET', 'POST', 'DELETE', 'PATCH'],
+    //     allowedHeaders: ['Content-Type', 'Authorization', 'x-access-token']
+    // })
+    // );
+
+
+    app.use(cors({ origin: '*', credentials: true ,methods: 'GET,POST,PUT,DELETE,PATCH' }));
+
     
     
     app.use("/api", appRoutes)
